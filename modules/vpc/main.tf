3f632499d8c2af0181ab8ab4408fcea5f1c45e50
Tunc Celik <tunc.celik@commencis.com>
2024-02-28 15:06:21 +0300

    feat 3

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -15,9 +15,10 @@ locals {
   cidr_part = trimsuffix(var.cidr, ".0.0")
 }
 
+#vpc module has been upgraded.
 module "vpc" {
   source                 = "terraform-aws-modules/vpc/aws"
-  version                = "3.14.0"
+  version                = "5.5.2"
   name                   = "${var.deployment_prefix}-VPC"
   cidr                   = "${var.cidr}/16"
   azs                    = data.aws_availability_zones.available.names
@@ -25,5 +26,4 @@ module "vpc" {
   public_subnets         = ["${local.cidr_part}.21.0/24", "${local.cidr_part}.22.0/24", "${local.cidr_part}.23.0/24"]
   enable_nat_gateway     = false
   single_nat_gateway     = true
-  one_nat_gateway_per_az = false
 }
